2efd1edfa03246dce773c4858ebb7560fbbb25a1
Freddie Rorison <freddieror@gmail.com>
2023-04-03 13:21:44 +0100

    New Database Structure

--- sql/main.sql
+++ sql/main.sql
@@ -18,37 +18,91 @@ CREATE TABLE Users (
     PRIMARY KEY (UserID)
 );
 
-CREATE TABLE PhysicalData (
-    RecordID INT NOT NULL AUTO_INCREMENT,
-    HeartRate INT,
-    Temperature INT,
-    BloopPressure INT,
-    dates date,
-    UserID INT NOT NULL,
-    PRIMARY KEY (RecordID)
-);
-
-CREATE TABLE ExerciseData (
-    RecordID INT NOT NULL AUTO_INCREMENT,
-    Duriation DOUBLE,
-    TypeID INT,
-    Notes varchar(255),
-    dates date,
-    UserID INT NOT NULL,
-    PRIMARY KEY (RecordID)
-);
-
-CREATE TABLE MentalHealthData (
-    RecordID INT NOT NULL AUTO_INCREMENT,
-    Notes varchar(255),
-    dates date,
-    MoodScore INT(10),
-    UserID INT NOT NULL,
-    PRIMARY KEY (RecordID)
-);
-
-CREATE TABLE ExerciseTypes (
-    TypeID INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE HealthSection (
+    SectionID INT NOT NULL AUTO_INCREMENT,
     Name varchar(255),
-    PRIMARY KEY (ExerciseID)
+    PulseRate Boolean,
+    BodyTemp Boolean,
+    BloodPressure Boolean,
+    ResperationRate Boolean,
+    PRIMARY KEY (SectionID)
+);
+
+CREATE TABLE HealthActivity (
+    ActivityID INT NOT NULL AUTO_INCREMENT,
+    UserID INT,
+    SectionID INT,
+    RecordID INT,
+    date DATETIME,
+    PRIMARY KEY (ActivityID),
+    FOREIGN KEY (SectionID) REFERENCES HealthSection(SectionID),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+);
+
+CREATE TABLE PulseRateRecord (
+    RecordID INT NOT NULL AUTO_INCREMENT,
+    PulseRate INT,
+    ActivityID INT,
+    PRIMARY KEY (RecordID),
+    FOREIGN KEY (ActivityID) REFERENCES HealthActivity(ActivityID)
+);
+
+CREATE TABLE BodyTemperatureRecord (
+    RecordID INT NOT NULL AUTO_INCREMENT,
+    BodyTemperature INT,
+    ActivityID INT,
+    PRIMARY KEY (RecordID),
+    FOREIGN KEY (ActivityID) REFERENCES HealthActivity(ActivityID)
+);
+
+CREATE TABLE BloodPressureRecord (
+    RecordID INT NOT NULL AUTO_INCREMENT,
+    Systollic INT,
+    Diastolic INT,
+    ActivityID INT,
+    PRIMARY KEY (RecordID),
+    FOREIGN KEY (ActivityID) REFERENCES HealthActivity(ActivityID)
+);
+
+CREATE TABLE ResperationRateRecord (
+    RecordID INT NOT NULL AUTO_INCREMENT,
+    ResperationRate INT,
+    ActivityID INT,
+    PRIMARY KEY (RecordID),
+    FOREIGN KEY (ActivityID) REFERENCES HealthActivity(ActivityID)
+);
+
+CREATE TABLE ExerciseCategory (
+    ID INT NOT NULL AUTO_INCREMENT,
+    Name varchar(255),
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE ExerciseRecord (
+    ID INT NOT NULL AUTO_INCREMENT,
+    UserID INT,
+    CategoryID INT,
+    Duriation INT,
+    Date DATETIME,
+    Notes varchar(255),
+    PRIMARY KEY (ID),
+    FOREIGN KEY (CategoryID) REFERENCES ExerciseCategory(ID),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+);
+
+CREATE TABLE MedicineType (
+    ID INT NOT NULL AUTO_INCREMENT,
+    Name varchar(255),
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE MedicationTracker (
+    ID INT NOT NULL AUTO_INCREMENT,
+    UserID INT,
+    MedicineID INT,
+    Period INT,
+    Dosage INT,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (MedicineID) REFERENCES MedicineType(ID),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
