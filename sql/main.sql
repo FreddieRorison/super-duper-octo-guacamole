dab49445e04f96a2fcf88616ac7ca92da599bc20
Freddie Rorison <freddieror@gmail.com>
2023-04-15 23:16:51 +0100

    Added test data

--- sql/main.sql
+++ sql/main.sql
@@ -31,8 +31,8 @@ CREATE TABLE HealthActivity (
     SectionID INT,
     date DATETIME,
     PRIMARY KEY (ActivityID),
-    FOREIGN KEY (SectionID) REFERENCES HealthSection(SectionID),
-    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+    FOREIGN KEY (SectionID) REFERENCES HealthSection(SectionID) ON DELETE CASCADE,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID) ON DELETE CASCADE
 );
 
 CREATE TABLE PulseRateRecord (
@@ -40,7 +40,7 @@ CREATE TABLE PulseRateRecord (
     PulseRate INT,
     ActivityID INT,
     PRIMARY KEY (RecordID),
-    FOREIGN KEY (ActivityID) REFERENCES HealthActivity(ActivityID)
+    FOREIGN KEY (ActivityID) REFERENCES HealthActivity(ActivityID) ON DELETE CASCADE
 );
 
 CREATE TABLE BodyTemperatureRecord (
@@ -48,7 +48,7 @@ CREATE TABLE BodyTemperatureRecord (
     BodyTemperature INT,
     ActivityID INT,
     PRIMARY KEY (RecordID),
-    FOREIGN KEY (ActivityID) REFERENCES HealthActivity(ActivityID)
+    FOREIGN KEY (ActivityID) REFERENCES HealthActivity(ActivityID) ON DELETE CASCADE
 );
 
 CREATE TABLE BloodPressureRecord (
@@ -57,7 +57,7 @@ CREATE TABLE BloodPressureRecord (
     Diastolic INT,
     ActivityID INT,
     PRIMARY KEY (RecordID),
-    FOREIGN KEY (ActivityID) REFERENCES HealthActivity(ActivityID)
+    FOREIGN KEY (ActivityID) REFERENCES HealthActivity(ActivityID) ON DELETE CASCADE
 );
 
 CREATE TABLE ResperationRateRecord (
@@ -65,7 +65,7 @@ CREATE TABLE ResperationRateRecord (
     ResperationRate INT,
     ActivityID INT,
     PRIMARY KEY (RecordID),
-    FOREIGN KEY (ActivityID) REFERENCES HealthActivity(ActivityID)
+    FOREIGN KEY (ActivityID) REFERENCES HealthActivity(ActivityID) ON DELETE CASCADE
 );
 
 CREATE TABLE ExerciseCategory (
@@ -82,8 +82,8 @@ CREATE TABLE ExerciseRecord (
     Date DATETIME,
     Notes varchar(255),
     PRIMARY KEY (ID),
-    FOREIGN KEY (CategoryID) REFERENCES ExerciseCategory(ID),
-    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+    FOREIGN KEY (CategoryID) REFERENCES ExerciseCategory(ID) ON DELETE CASCADE,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID) ON DELETE CASCADE
 );
 
 CREATE TABLE MedicineType (
@@ -99,6 +99,26 @@ CREATE TABLE MedicationTracker (
     Period INT,
     Dosage INT,
     PRIMARY KEY (ID),
-    FOREIGN KEY (MedicineID) REFERENCES MedicineType(ID),
-    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+    FOREIGN KEY (MedicineID) REFERENCES MedicineType(ID) ON DELETE CASCADE,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID) ON DELETE CASCADE
 );
+
+INSERT INTO Users (Firstname, Surname, Email, Password, DateOfBirth) VALUES ("Freddie", "Poop", "test@test.com", "farting", "2023-03-02");
+
+INSERT INTO HealthSection (Name, PulseRate, BodyTemp, BloodPressure, ResperationRate) VALUES ("Silly Goofy ah ah", 1,1,0,1);
+INSERT INTO HealthActivity (UserID, SectionID, date) VALUES (1,1,"2022-1-1");
+INSERT INTO BodyTemperatureRecord (BodyTemperature, ActivityID) VALUES (39,1);
+INSERT INTO PulseRateRecord (PulseRate, ActivityID) VALUES (85,1);
+INSERT INTO ResperationRateRecord (ResperationRate, ActivityID) VALUES (15,1);
+
+INSERT INTO HealthActivity (UserID, SectionID, date) VALUES (1,1,"2022-2-1");
+INSERT INTO BodyTemperatureRecord (BodyTemperature, ActivityID) VALUES (40,2);
+INSERT INTO PulseRateRecord (PulseRate, ActivityID) VALUES (87,2);
+INSERT INTO ResperationRateRecord (ResperationRate, ActivityID) VALUES (17,2);
+
+INSERT INTO HealthSection (Name, PulseRate, BodyTemp, BloodPressure, ResperationRate) VALUES ("BP gooooooo", 1,1,1,1);
+INSERT INTO HealthActivity (UserID, SectionID, date) VALUES (1,2,"2022-1-1");
+INSERT INTO BodyTemperatureRecord (BodyTemperature, ActivityID) VALUES (39,2);
+INSERT INTO PulseRateRecord (PulseRate, ActivityID) VALUES (85,2);
+INSERT INTO ResperationRateRecord (ResperationRate, ActivityID) VALUES (15,2);
+INSERT INTO BloodPressureRecord (Systollic, Diastolic, ActivityID) VALUES (120,80,2);
